fc618d79bfa3aba8a76c21e7694da07ec4055dcf
Wolfgang Kubisiak <wolfgang@kubisiak.de>
2026-01-08 10:33:40 +0100

    cleanup: Remove misplaced Inventory directory from appointment-manager

--- appointment-manager/Inventory/README.md
+++
@@ -1,7 +0,0 @@
-# Inventory Tracker - Demo App 1
-
-## Live Demo: [URL]
-
-## Loom: [Video-Link]
-
-## BDD Specs: 5/5 passing
--- appointment-manager/Inventory/backend/jest.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  cacheDirectory: './node_modules/.cache/jest',
-  testEnvironment: 'node',
-  transform: {}
-};
-
